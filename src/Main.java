e08ec2034eff76261e8b27b2671cbb73bf384999
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-10-22 14:26:33 -0300

    String interpolation challenge

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,24 @@ public class Main {
         System.out.println(numberOne); // Printing just the number
         System.out.printf("%.2f%n", numberOne); // Printing the number with 2 decimal places
         System.out.printf("%.4f%n", numberOne); // Printing the number with 4 decimal places
+
+
+//        String manipulation exercise
+        String productOne = "Computer";
+        String productTwo = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+
+        double priceOne = 2100.0;
+        double priceTwo = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.println(productOne + ", " + "whose price is $" + priceOne);
+        System.out.println(productTwo + ", " + "whose price is $" + priceTwo);
+        System.out.println("Record: " + age + " years old, code " + code + " and gender F");
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded to three places: %.3f%n", measure);
     }
 }
