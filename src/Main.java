ea69eeb3554e3e6b3dd462abf59b39123960cc01
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-10-22 14:10:54 -0300

    Printing numbers and assigning variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+//        Printing formatted numbers and assigning variables
+        double numberOne = 1.12345;
+        System.out.println(numberOne); // Printing just the number
+        System.out.printf("%.2f%n", numberOne); // Printing the number with 2 decimal places
+        System.out.printf("%.4f%n", numberOne); // Printing the number with 4 decimal places
     }
 }
