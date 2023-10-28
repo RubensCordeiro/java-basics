121b635479dcf81d739c3527ec8e40f7269a38ac
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-10-28 11:23:11 -0300

    Changed product logic to work with classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,22 +16,37 @@ public class Main {
         System.out.printf("%.4f%n", numberOne); // Printing the number with 4 decimal places
 
 
-//        String manipulation exercise
-        String productOne = "Computer";
-        String productTwo = "Office desk";
+//        String manipulation exercise - now with classes
+        Product productOne = new Product();
+        productOne.quantity = 1;
+        productOne.price = 2100;
+        productOne.name = "Computer";
+
+        Product productTwo = new Product();
+        productTwo.quantity = 1;
+        productTwo.price = 650.50;
+        productTwo.name = "Office desk";
+
 
         int age = 30;
         int code = 5290;
 
-        double priceOne = 2100.0;
-        double priceTwo = 650.50;
         double measure = 53.234567;
 
         System.out.println("Products:");
-        System.out.println(productOne + ", " + "whose price is $" + priceOne);
-        System.out.println(productTwo + ", " + "whose price is $" + priceTwo);
+        System.out.println(productOne.toString());
+        System.out.println(productTwo.name + ", " + "whose price is $" + productTwo.price);
         System.out.println("Record: " + age + " years old, code " + code + " and gender F");
         System.out.printf("Measure with eight decimal places: %.8f%n", measure);
         System.out.printf("Rounded to three places: %.3f%n", measure);
+
+//        Dealing with user input
+/*        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your number");
+
+        int input = sc.nextInt();
+        System.out.println("You have typed: " + input);
+
+        sc.close();*/
     }
 }
